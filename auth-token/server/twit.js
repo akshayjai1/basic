290f5c28f3a1b64286addeb04cc3ee67904c30d3
Akshay Vijay Jain <akshayj3@hexaware.com>
2020-06-06 15:23:26 +0530

    twitter

--- auth-token/server/twit.js
+++ auth-token/server/twit.js
@@ -2,7 +2,7 @@ const Twitter = require("twitter-lite");
 // console.log(Twitter);
 const config = {
   //   subdomain: "cors-anywhere.herokuapp.com/https://api",
-  consumer_key: "bFfvsWCzFPy519lNeQoBb1Hzd",
+  consumer_key: "l7ZOSz5VwmfgiQBVEXT8PvZ2O",
   consumer_secret: "Ea1GCJueDZ3tHpiZsMbQLtJvMTDfZf2IRc1SX5GQWvJe7o0kw1",
   access_token: "1681758896-7b6KrORKHYJLkcntSUo7yFTaA6oFOOllzL8PWo2",
   access_token_secret: "ZjXEmBbi9vpMEoSjkzKYLDtJ3Msk61i5tY0e7QSB2FKvI",
