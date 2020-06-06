02a4e9b58f08cbac258bf40c046ddb089cebd9ec
Akshay Vijay Jain <akshayj3@hexaware.com>
2020-06-06 15:24:17 +0530

    updated security token for twitter

--- auth-token/server/twit.js
+++ auth-token/server/twit.js
@@ -3,9 +3,9 @@ const Twitter = require("twitter-lite");
 const config = {
   //   subdomain: "cors-anywhere.herokuapp.com/https://api",
   consumer_key: "l7ZOSz5VwmfgiQBVEXT8PvZ2O",
-  consumer_secret: "Ea1GCJueDZ3tHpiZsMbQLtJvMTDfZf2IRc1SX5GQWvJe7o0kw1",
-  access_token: "1681758896-7b6KrORKHYJLkcntSUo7yFTaA6oFOOllzL8PWo2",
-  access_token_secret: "ZjXEmBbi9vpMEoSjkzKYLDtJ3Msk61i5tY0e7QSB2FKvI",
+  consumer_secret: "pPpamsLn8ZUZaI1fMDhz1o9w00wx3kDTl4r8Cun4ajdzxon7RQ",
+  access_token: "1681758896-1681758896-ossFSOrs38ZZRrjRG9fBG8Qpb9RB7H3LwUoela2",
+  access_token_secret: "m9AO59jF0Y6g8Wi1DplcBtpbEMuhJbK49Irp4Rk4dtffd",
   // timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
   // strictSSL:            true,     // optional - requires SSL certificates to be valid.
 };
